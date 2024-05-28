662b4d208b2bf3082688992322e1aaa258d9e293
funnyLittleGoober <carlos.silva1704@aluno.ce.gov.br>
2024-05-28 01:37:03 -0300

    "Updated sign in/up form"

--- signInSignUpLogOut/formSignIn.php
+++ signInSignUpLogOut/formSignIn.php
@@ -1,6 +1,14 @@
-<?php
-require '../header.php';
-?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistema de Achados & Perdidos</title>
+  <link rel="stylesheet" href="../style.css">
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="d-flex align-items-center">
   <?php
   // Se a inscrição for bem-sucedida, o usuário será direcionado para a página de login que conterá uma mensagem para garantir que a conta foi criada com sucesso
   if(isset($_GET['signUpSuccess'])){
@@ -8,7 +16,7 @@ require '../header.php';
       echo 'Successfully signed up!';
     }
   }
-  // ISe o login falhar o usuário será direcionado para a página de login que terá uma mensagem esclarecendo que as credenciais utilizadas são inválidas
+  // Se o login falhar o usuário será direcionado para a página de login que terá uma mensagem esclarecendo que as credenciais utilizadas são inválidas
   if(isset($_GET['signInError'])){
     if($_GET['signInError'] === 'yes'){
       echo 'Invalid credentials.';
@@ -21,25 +29,31 @@ require '../header.php';
     }
   }
 ?>
-  <div class="w-100 m-auto"></div>
-  <img src="../assets/logo.jpg" alt="logo" class="max-w-">
-  <h2 class="mb-3">Login</h2>
+  <div class="w-100 m-auto form-container">
   <form action="signInValidation.php" method="post">
-    <div class="mb-3">
-      <label for="username" class="form-label">Username:</label>
-      <input name="username" id="username" class="form-control">
+    <img src="../assets/trimmedLogo.png" alt="logo" class="w-100 m-auto form-logo d-block mb-2">
+    <span class="h3 fw-semi-bold">Login,</span>
+    <span class="h5 fw-semi-bold text-warning"> olá denovo!</span>
+    <div class="form-floating mt-3">
+      <input name="username" id="username" class="form-control mb-1" placeholder="Nome de usuário">
+      <label for="username">Nome de usuário</label>
     </div>
-    <div class="mb-3">
-      <label for="email" class="form-label">Email address:</label>
-      <input type="email" name="email" id="email" class="form-control">
+    <div class="form-floating">
+      <input type="email" name="email" id="email" class="form-control mb-1" placeholder="seu-email@gmail.com">
+      <label for="email">E-mail</label>
     </div>
-    <div class="mb-3">
-      <label for="password" class="form-label">Password:</label>
-      <input type="password" name="password" id="password" class="form-control">
+    <div class="form-floating">
+      <input type="password" name="password" id="password" class="form-control" placeholder="Senha">
+      <label for="password">Senha</label>
     </div>
-    <input type="submit" name="submit" value="Submit" class="mb-3 btn btn-warning">
+    <input type="submit" name="submit" value="Submit" class="my-3 btn btn-warning w-100">
+    <div class="text-center">
+      <span>Ainda não tem uma conta? • </span><a href="formSignUp.php" class="text-warning">Cadastre-se</a>
+    </div>
+    <hr>
+    <p class="text-secondary copyright text-center">Direitos Autorais © 2024 Carlos Silva. Todos os Direitos Reservados</p> 
   </form>
-  <a href="formSignUp.php" class="">Ainda não se cadastrou?</a>
+  </div>
 <?php
 require '../footer.php';
 ?>
--- signInSignUpLogOut/formSignUp.php
+++ signInSignUpLogOut/formSignUp.php
@@ -1,6 +1,14 @@
-<?php
-require '../header.php';
-?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistema de Achados & Perdidos</title>
+  <link rel="stylesheet" href="../style.css">
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="d-flex align-items-center">
   <?php
   // Caso o usuário envie apenas espaço em um dos inputs, ele será enviado de volta ao formulário de cadastro com uma mensagem esclarecendo que as credenciais utilizadas são inválidas
   if(isset($_GET['signUpError'])){
@@ -9,23 +17,31 @@ require '../header.php';
     }
   }
   ?>
-  <h2 class="mb-3">Cadastro</h2>
+  <div class="w-100 m-auto form-container">
   <form action="signUpValidation.php" method="post">
-    <div class="mb-3">
-      <label for="username">Username:</label>
-      <input name="username" id="username">
+    <img src="../assets/trimmedLogo.png" alt="logo" class="w-100 m-auto form-logo d-block mb-2">
+    <span class="h3 fw-semi-bold">Cadastro,</span>
+    <span class="h5 fw-semi-bold text-warning"> bem-vindo(a)!</span>
+    <div class="form-floating mt-3">
+      <input name="username" id="username" class="form-control mb-1" placeholder="Nome de usuário">
+      <label for="username">Nome de usuário</label>
     </div>
-    <div class="mb-3">
-      <label for="email">Email address:</label>
-      <input type="email" name="email" id="email">
+    <div class="form-floating">
+      <input type="email" name="email" id="email" class="form-control mb-1" placeholder="seu-email@gmail.com">
+      <label for="email">E-mail</label>
     </div>
-    <div class="mb-3">
-      <label for="password">Password:</label>
-      <input type="password" name="password" id="password">
+    <div class="form-floating">
+      <input type="password" name="password" id="password" class="form-control" placeholder="Senha">
+      <label for="password">Senha</label>
     </div>
-    <input type="submit" name="submit" value="Submit" class="mb-3">
+    <input type="submit" name="submit" value="Submit" class="my-3 btn btn-warning w-100">
+    <div class="text-center">
+      <span>Já tem uma conta? • </span><a href="formSignIn.php" class="text-warning">Login</a>
+    </div>
+    <hr>
+    <p class="text-secondary copyright text-center">Direitos Autorais © 2024 Carlos Silva. Todos os Direitos Reservados</p>
   </form>
-  <a href="formSignIn.php">Já tem uma conta?</a>
+  </div>
 <?php
 require '../footer.php';
 ?>
