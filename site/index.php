90f51e58798a1b444cfdcf5d163e4a3069074d95
DevCarlosSilva <carlos.silva1704@aluno.ce.gov.br>
2024-06-06 13:45:10 -0300

    Added nav/side bar

--- site/index.php
+++ site/index.php
@@ -7,46 +7,53 @@ require 'template/header.php';
 ?>
 
 <body>
-  <!-- sidebar e dropup + navbar com nome da página e logo -->
-  <nav class="navbar logo-bg-color">
-    <div class="container-fluid d-flex justify-content-start">
-      <button class="navbar-toggler me-2" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        <ion-icon name="menu"></ion-icon>
-      </button>
+  <nav class="navbar logo-bg-color px-4">
+    <div class="container-fluid d-flex justify-content-between align-items-center p-0">
       <div class="navbar-brand">
-        <img src="assets/noTitleLogo.png" width="35" height="35" alt="imagem da logo" class="mx-2" draggable="false" />
-        <img src="assets/onlyTitleLogo.png" width="140" height="24" alt="nome da logo" draggable="false" />
+        <ion-icon name="menu-outline" class="btn sidebar-toggle" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></ion-icon>
+        <a class="logo-navbar">
+          <img src="assets/noTitleLogo.png" width="35" height="35" alt="imagem da logo" class="me-2" draggable="false" />
+          <img src="assets/onlyTitleLogo.png" width="140" height="24" alt="nome da logo" draggable="false" />
+        </a>
+      </div>
+      <div class="page-identificator d-flex align-items-center">
+        <ion-icon name="home-outline" class="me-2 page-identificator-icon"></ion-icon>
+        <div class="h4 m-0">Página inicial</div>
       </div>
     </div>
   </nav>
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen">
-      <div class="modal-content">
-        <div class="modal-header">
-          <div class="modal-title mx-auto" id="exampleModalLabel">
-            <img src="assets/noTitleLogo.png" width="35" height="35" alt="imagem da logo" class="me-2" draggable="false" />
-            <img src="assets/onlyTitleLogo.png" width="140" height="24" alt="nome da logo" draggable="false" />
-          </div>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item rounded">An item</li>
-            <li class="list-group-item rounded">A second item</li>
-            <li class="list-group-item rounded">A third item</li>
-            <li class="list-group-item rounded">A fourth item</li>
-            <li class="list-group-item rounded">And a fifth one</li>
-          </ul>
-        </div>
-        <div class="modal-footer">
-          <a class="btn d-flex align-items-center log-out-button">
-            <ion-icon name="log-out-outline" class="me-1"></ion-icon>Sair da
-            conta
-          </a>
-        </div>
+
+  <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+    <div class="offcanvas-header d-flex align-items-center">
+      <img src="assets/noTitleLogo.png" width="35" height="35" alt="imagem da logo" class="me-2" draggable="false" />
+      <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Menu</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      //! REQUIRE ISSUE
+      <ul class="list-group list-group-flush">
+        <a class="list-group-item rounded" href="index.html">Página inicial</a>
+        <a class="list-group-item rounded" href="index.html">Itens perdidos</a>
+        <a class="list-group-item rounded" href="index.html">Itens devolvidos</a>
+      </ul>
+    </div>
+    <div class="offcanvas-footer h-100 d-flex align-items-center justify-content-end px-3">
+      <div class="btn-group dropup">
+        <button type="button" class="btn dropdown-toggle d-flex align-items-center account-settings-button" data-bs-toggle="dropdown" aria-expanded="false">
+          <ion-icon name="person-circle-outline" class="me-1 account-settings-icon"></ion-icon><?php echo $_SESSION['username']; ?>
+        </button>
+        <ul class="dropdown-menu">
+          <li>
+            //! REQUIRE ISSUE
+            <a class="btn d-flex align-items-center dropdown-item log-out-button" href="index.html">
+              <ion-icon name="log-out-outline" class="me-1 log-out-icon"></ion-icon>Sair da conta
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
+
   <link rel="stylesheet" href="css/style.css">
   <?php
   require 'template/footer.php';
